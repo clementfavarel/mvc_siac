ada48c207f2dd64c3c76e43f72a9d2cfa0e32019
clementfavarel <plkode.dev@protonmail.com>
2023-12-29 21:40:00 +0100

    updated landing page

--- src/views/landing.php
+++ src/views/landing.php
@@ -6,19 +6,24 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Accueil | App</title>
+    <title>Accueil | Mapollon</title>
     <link rel="stylesheet" href="assets/css/landing.css" />
 </head>
 
 <body>
-    <div class="card">
-        <h1>Bienvenue sur App</h1>
-        <p>Pour découvrir l'app et profiter de toutes ses fonctionnalités, veuillez vous inscrire.</p>
-        <a class="btn" href="index.php?action=register">Créer mon compte</a>
-        <p class="small">
-            Vous possédez déjà un compte ?<br />
-            <a href="index.php?action=login">Connectez-vous</a>
-        </p>
+    <div class="container">
+        <div class="images">
+            <img class="siac" src="assets/img/logo-siac.svg" alt="SIAC" />
+            <img class="mapollon" src="assets/img/logo-mapollon.svg" alt="MAP'OLLON" />
+        </div>
+        <div class="content">
+            <h1 class="title">visitez le siac avec map'ollon</h1>
+            <p class="text">Arpentez le salon à travers un parcours sur les artistes engagés, avec de la Réalité Augmenté, des interviews personnalisées, ambiances sonores, et plus encore !</p>
+        </div>
+        <div class="buttons">
+            <a class="btn secondary" href="index.php?action=login">Je me connecte</a>
+            <a class="btn primary" href="index.php?action=register">Je crée mon compte</a>
+        </div>
     </div>
 </body>
 
