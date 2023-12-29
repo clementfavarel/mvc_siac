fa605bb37aa38376a8729a217e04b5e041cfb801
clementfavarel <plkode.dev@protonmail.com>
2023-12-29 22:35:35 +0100

    updated register page

--- src/views/auth/register.php
+++ src/views/auth/register.php
@@ -6,37 +6,60 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Inscription | App</title>
+    <title>Inscription | Mapollon</title>
+    <link rel="stylesheet" href="assets/css/auth.css" />
     <link rel="stylesheet" href="assets/css/register.css" />
 </head>
 
 <body>
-    <div class="card">
+    <a href="index.php" class="previous">
+        <img src="assets/icons/previous.svg" alt="<=" />
+    </a>
+    <div class="container">
         <h1>Inscription</h1>
-        <form action="index.php?action=register" method="post">
-            <div class="input-group">
-                <label for="user_name">Nom d'utilisateur</label>
-                <input type="text" name="user_name" id="user_name" required />
+        <form method="post" id="regForm" action="index.php?action=register">
+            <!-- One "tab" for each step in the form: -->
+            <div class="tab">
+                <label for="user_firstname">Prénom</label>
+                <input oninput="this.className = ''" name="user_firstname" />
+                <label for="user_lastname">Nom</label>
+                <input oninput="this.className = ''" name="user_lastname" />
             </div>
-            <div class="input-group">
+            <div class="tab">
+                <label for="user_job">Profession</label>
+                <input oninput="this.className = ''" name="user_job" />
                 <label for="user_email">Email</label>
-                <input type="email" name="user_email" id="user_email" required />
+                <input type="email" oninput="this.className = ''" name="user_email" />
             </div>
-            <div class="input-group">
+            <div class="tab">
+                <label for="birth">Date de naissance</label>
+                <div class="birth">
+                    <input placeholder="jj" oninput="this.className = ''" name="dd" />
+                    <input placeholder="mm" oninput="this.className = ''" name="mm" />
+                    <input placeholder="aaaa" oninput="this.className = ''" name="yyyy" />
+                </div>
+            </div>
+            <div class="tab">
                 <label for="user_pwd">Mot de passe</label>
-                <input type="password" name="user_pwd" id="user_pwd" required />
+                <input oninput="this.className = ''" name="user_pwd" type="password" />
+                <label for="user_pwd_confirm">Confirmer le mot de passe</label>
+                <input oninput="this.className = ''" name="user_pwd_confirm" type="password" />
             </div>
-            <div class="input-group">
-                <label for="user_pwd_repeat">Confirmer le mot de passe</label>
-                <input type="password" name="user_pwd_repeat" id="user_pwd_repeat" required />
+            <!-- Circles which indicates the steps of the form: -->
+            <div class="dots">
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
             </div>
-            <input class="btn" type="submit" name="submit" value="Créer mon compte" />
+            <div class="btns">
+                <button class="btn primary" type="button" id="nextBtn" onclick="nextPrev(1)">Suivant</button>
+                <button class="btn secondary" type="button" id="prevBtn" onclick="nextPrev(-1)">Précédent</button>
+            </div>
         </form>
-        <p class="small">
-            Vous possédez déjà un compte ?<br />
-            <a href="index.php?action=login">Connectez-vous</a>
-        </p>
     </div>
+
+    <script src="assets/js/register-form.js"></script>
 </body>
 
 </html>
