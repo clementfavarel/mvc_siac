2f51d4b6a428cc5b12dfbfd7b618ca4c21e8f446
Alan THOB <alan.thob@hotmail.fr>
2024-01-26 15:07:35 +0100

    Update profil and errors UI

--- src/views/app/profile.php
+++ src/views/app/profile.php
@@ -2,15 +2,17 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Profil | Mapollon</title>
-    <link rel="stylesheet" href="assets/css/profile.css" />
-    <link rel="stylesheet" href="assets/css/tab-bar.css" />
+    <link rel="stylesheet" href="assets/css/profile.css"/>
+    <link rel="stylesheet" href="assets/css/tab-bar.css"/>
 </head>
+<body>
 
 <div class="user-banner">
     <h2>Bonjour, <?php echo $userData['user_firstname'] ?></h2>
+    <a href="index.php?action=logout"><img src="assets/icons/power.svg"></a>
 </div>
 
 <div class="user-infos-section">
@@ -37,26 +39,19 @@
     </div>
 
     <div class="inputs">
-        <input type="password" placeholder=Confirmer le mot de passe">
+        <input type="password" placeholder="Confirmer le mot de passe">
     </div>
 
-    <div class="disconnect">
-        <a id="logout-btn" href="index.php?action=logout"><img src="assets/icons/power.svg">Sauvegarder</a>
+    <div class="save">
+        <button type="submit"><span><img src="assets/icons/save.svg">Sauvegarder</span></button>
     </div>
 
-    <button type="submit">Sauvegarder</button>
 </form>
 
 
-    <div class="disconnect">
-        <a id="logout-btn" href="index.php?action=logout"><img src="assets/icons/power.svg">Se déconnecter</a>
-    </div>
+<?php include 'assets/includes/tab-bar.php'; ?>
+<script src="assets/js/tab-bar.js"></script>
 
-</div>
-
-
-    <?php include 'assets/includes/tab-bar.php'; ?>
-    <script src="assets/js/tab-bar.js"></script>
 </body>
 
 </html>
