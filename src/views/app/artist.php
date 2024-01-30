99b6337331eb3eddf0fed17de75f4e8dc8757c33
Alan THOB <alan.thob@hotmail.fr>
2024-01-30 15:46:49 +0100

    Add another carousel artist page

--- src/views/app/artist.php
+++ src/views/app/artist.php
@@ -5,90 +5,93 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Artiste | Mapollon</title>
     <link rel="stylesheet" href="assets/css/slick.css">
     <link rel="stylesheet" href="assets/css/slick-theme.css">
-    <link rel="stylesheet" href="assets/css/artist.css" />
-    <link rel="stylesheet" href="assets/css/tab-bar.css" />
+    <link rel="stylesheet" href="assets/css/artist.css"/>
+    <link rel="stylesheet" href="assets/css/tab-bar.css"/>
 </head>
 
 <body>
-    <div class="container">
-        <div class="bg" style="background-image: url('<?= $artistData['bg_src'] ?>');background-repeat: no-repeat; background-size: cover">
-            <header>
-                <a class="previous" href="index.php?action=map">
-                    <img src="assets/icons/previous.svg" alt="Retour" />
-                </a>
-                <h3>Stand n°<?= $artistData['num_stand'] ?></h3>
-            </header>
-            <div class="profile">
-                <img class="picture" src="assets/img/aklarousse/cocotte-barcelone.JPEG" alt="La Cocotte de Barcelone" />
-                <h2 class="aw-title">la cocotte de barcelone</h2>
-                <h1 class="at-name"><?= $artistData['pseudo'] ?></h1>
-            </div>
-            <div class="video">
-                <img class="plume-haute" src="<?= $artistData['deco1'] ?>" alt="deco1" />
-                <video class="media" controls>
-                    <source src="<?= $artistData['media_src'] ?>" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-                <img class="plume-basse" src="<?= $artistData['deco2'] ?>" alt="deco2" />
-            </div>
-            <div class="separator">
-                <hr class="big">
-                <hr class="small">
-            </div>
-
-            <section class="lazy slider">
-                <div>
-                    <h4>Qui est <?= $artistData['pseudo'] ?> ?</h4>
-                    <p><?= $artistData['description1'] ?></p>
-                </div>
-                <div>
-                    <h4>Quelles sont ses ambitions ?</h4>
-                    <p><?= $artistData['description2'] ?></p>
-                </div>
-                <div>
-                    <h4>Quel est son travail ?</h4>
-                    <p><?= $artistData['description3'] ?></p>
-                </div>
-            </section>
-
-            <div class="separator">
-                <hr class="small">
-                <hr class="big">
-            </div>
+<div class="container">
+    <div class="bg"
+         style="background-image: url('<?= $artistData['bg_src'] ?>');background-repeat: no-repeat; background-size: cover">
+        <header>
+            <a class="previous" href="index.php?action=map">
+                <img src="assets/icons/previous.svg" alt="Retour"/>
+            </a>
+            <h3>Stand n°<?= $artistData['num_stand'] ?></h3>
+        </header>
+        <div class="profile">
+            <img class="picture" src="assets/img/aklarousse/cocotte-barcelone.JPEG" alt="La Cocotte de Barcelone"/>
+            <h2 class="aw-title">la cocotte de barcelone</h2>
+            <h1 class="at-name"><?= $artistData['pseudo'] ?></h1>
         </div>
-        <div class="artwork">
-            <h2 class="aw-title"><?= $artworkData['title'] ?></h2>
-            <img class="artwork-img" src="<?= $artworkData['image_src'] ?>" alt="<?= $artworkData['image_alt'] ?>" />
+        <div class="video">
+            <img class="plume-haute" src="<?= $artistData['deco1'] ?>" alt="deco1"/>
+            <video class="media" controls>
+                <source src="<?= $artistData['media_src'] ?>" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+            <img class="plume-basse" src="<?= $artistData['deco2'] ?>" alt="deco2"/>
         </div>
-        <div class="anecdote"></div>
-        <h4 class="aw-title">ses oeuvres</h4>
-        <div class="see-also">
-            <img src="assets/icons/chevron-left.svg" alt="<" />
-            <div class="text">
-                <div class="tab">
-                </div>
+        <div class="separator">
+            <hr class="big">
+            <hr class="small">
+        </div>
+
+        <section class="lazy slider">
+            <div>
+                <h4>Qui est <?= $artistData['pseudo'] ?> ?</h4>
+                <p><?= $artistData['description1'] ?></p>
             </div>
-            <img src="assets/icons/chevron-right.svg" alt=">" />
+            <div>
+                <h4>Quelles sont ses ambitions ?</h4>
+                <p><?= $artistData['description2'] ?></p>
+            </div>
+            <div>
+                <h4>Quel est son travail ?</h4>
+                <p><?= $artistData['description3'] ?></p>
+            </div>
+        </section>
+
+        <div class="separator">
+            <hr class="small">
+            <hr class="big">
         </div>
     </div>
+    <div class="artwork">
+        <h2 class="aw-title"><?= $artworkData['title'] ?></h2>
+        <img class="artwork-img" src="<?= $artworkData['image_src'] ?>" alt="<?= $artworkData['image_alt'] ?>"/>
+    </div>
+    <div class="anecdote"></div>
+    <h4 class="aw-title">ses oeuvres</h4>
+    <section class="lazy slider">
+        <div class="see-also">
+            <img class="artwork-img" src="assets/img/aklarousse/cocotte-barcelone.JPEG" alt="La Cocotte de Barcelone"/>
+            <h2>La Cocotte de Barcelone</h2>
+        </div>
+        <div>
+            <img class="artwork-img" src="assets/img/aklarousse/lilith.png" alt="La Cocotte de Barcelone"/>
+            <h2>Lilith</h2>
+        </div>
+    </section>
+</div>
 
-    <?php include 'assets/includes/tab-bar.php'; ?>
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-    <script src="assets/js/slick.js" type="text/javascript" charset="utf-8"></script>
-    <script type="text/javascript">
-        $(document).on('ready', function() {
-            $(".lazy").slick({
-                lazyLoad: 'ondemand',
-                infinite: true
-            });
+<?php include 'assets/includes/tab-bar.php'; ?>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="assets/js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+    $(document).on('ready', function () {
+        $(".lazy").slick({
+            lazyLoad: 'ondemand',
+            infinite: true
         });
-    </script>
-    <script src="assets/js/tab-bar.js"></script>
+    });
+</script>
+<script src="assets/js/tab-bar.js"></script>
 
 
 </body>
