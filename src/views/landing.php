4e3200765606cd6af22b7c489063e5f1c090aa57
Alan THOB <alan.thob@hotmail.fr>
2024-02-03 18:38:24 +0100

    Update auth

--- src/views/landing.php
+++ src/views/landing.php
@@ -4,32 +4,33 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Accueil | Mapollon</title>
-    <link rel="stylesheet" href="assets/css/landing.css" />
+    <link rel="stylesheet" href="assets/css/landing.css"/>
 </head>
 
 <body>
-    <div class="container">
-        <div class="images">
-            <img class="siac" src="assets/img/logo-siac.svg" alt="SIAC" />
-            <img class="mapollon" src="assets/img/logo-mapollon.svg" alt="MAP'OLLON" />
-        </div>
-        <div class="content">
-            <h1 class="title">visitez le siac avec map'ollon</h1>
-            <p class="text">
-
-                Découvrez une expérience immersive au SIAC, avec plus de 100 artistes engagés. Explorez des œuvres en Réalité Augmentée, plongez dans des interviews et ressentez des ambiances sonores. </p>
-        </div>
-        <div class="buttons">
-            <a class="btn primary" href="index.php?action=login">Je me connecte</a>
-            <a class="btn secondary" href="index.php?action=register">Je crée mon compte</a>
-            <a class="btn guest" href="index.php?action=map">Accéder au plan</a>
-            <p>Certaines fonctionnalités de l'application sont restreintes en mode invité.</p>
-
-        </div>
+<div class="container">
+    <div class="images">
+        <img class="siac" src="assets/img/logo-siac.svg" alt="SIAC"/>
+        <img class="mapollon" src="assets/img/logo-mapollon.svg" alt="MAP'OLLON"/>
     </div>
+    <div class="content">
+        <h1 class="title">visitez le siac avec map'ollon</h1>
+        <p class="text">
+
+            Découvrez une expérience immersive au SIAC, avec plus de 100 artistes engagés. Explorez des œuvres en
+            Réalité Augmentée, plongez dans des interviews et ressentez des ambiances sonores. </p>
+    </div>
+    <div class="buttons">
+        <a class="btn primary" href="index.php?action=login">Connexion</a>
+        <a class="btn secondary" href="index.php?action=register">Inscription</a>
+        <a class="btn guest" href="index.php?action=map">Mode invité</a>
+        <p>Certaines fonctionnalités de l'application sont restreintes en mode invité.</p>
+
+    </div>
+</div>
 </body>
 
 </html>
