f5d5f6c1b9b22752c3aac41cdcf07def1757f37c
clementfavarel <plkode.dev@protonmail.com>
2024-02-06 18:53:56 +0100

    removed map and related files

--- src/views/app/map.php
+++ src/views/app/map.php
@@ -7,28 +7,19 @@
     <title>Plan | Mapollon</title>
     <link rel="stylesheet" href="assets/css/map.css" />
     <link rel="stylesheet" href="assets/css/tab-bar.css" />
-    <script type="importmap">
-        {
-            "imports": {
-            "three": "https://unpkg.com/three@0.160.1/build/three.module.js",
-            "three/addons/": "https://unpkg.com/three@0.160.1/examples/jsm/"
-            }
-        }
-</script>
 </head>
 
 <body>
     <div class="top-bar">
         <div class="logo"><img src="assets/icons/logo.svg" alt="Map" class="mapollon" /></div>
-        <div class="filters">
+        <a class="filters">
             <img src="assets/icons/users.svg" alt="Artists" class="top_icon" />
-        </div>
+        </a>
     </div>
     <div class="map-container" id="map-container"></div>
 
     <?php include 'assets/includes/tab-bar.php'; ?>
     <script src="assets/js/tab-bar.js"></script>
-    <script type="module" src="assets/js/map.js"></script>
 </body>
 
 </html>
